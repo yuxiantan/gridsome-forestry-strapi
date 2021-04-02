580cf6c2704ceda947765c69671df5c9a300bc56
tanyuxian <tanyuxian@ridatatech.com>
2021-04-02 09:06:56 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c68e3f88e877790ab9145f09ddf12748'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6570b70b-b471-4929-84ba-9447e757ba4c'
+};
